d6cc7dda26f8e2564b39187f7b49f24eac7c27d4
brami007 <brami007@fiu.edu>
2021-01-12 00:14:53 -0500

    final changes

--- MySeleniumExample/src/test/java/com/mySeleniumExample/ExampleCase1.java
+++ MySeleniumExample/src/test/java/com/mySeleniumExample/ExampleCase1.java
@@ -31,56 +31,6 @@ public class ExampleCase1 {
 		Thread.sleep(1500);
 		
 		
-		//2. accept and validate alerts
-		myDriver = new ChromeDriver();
-		pageobjectmodelexample = new PageObjectModelExample(myDriver);
-		myDriver.navigate().to("http://demo.guru99.com/test/delete_customer.php");
-		
-		//Submit customer to delete
-		pageobjectmodelexample.sendKeysToElement("name", "cusid", "123456");
-		pageobjectmodelexample.clickElement("name", "submit");
-		
-		//accept alert
-		Alert alert = myDriver.switchTo().alert();
-		alert.accept();
-		
-		
-		//Verify alert deleted successfully
-		//alert = myDriver.switchTo().alert();
-		String alertMessage= myDriver.switchTo().alert().getText();
-		Assert.assertEquals(alertMessage,"Customer Successfully Delete!");
-		
-		myDriver.close();
-		Thread.sleep(1500);
-		
-		
-		//3. handling multiple windows and verify all windows closed
-		myDriver = new ChromeDriver();
-		pageobjectmodelexample = new PageObjectModelExample(myDriver);
-		myDriver.navigate().to("http://www.naukri.com/");
-		
-		String main = myDriver.getWindowHandle();
-		
-		//get the number of pop ups currently open and close each window
-		Set<String> set = myDriver.getWindowHandles();
-
-		Iterator<String> itr= set.iterator();
-		while(itr.hasNext()){
-			String childWindow=itr.next();
-			if(!main.equals(childWindow)){
-			myDriver.switchTo().window(childWindow);
-			myDriver.close();
-			}
-		}
-		
-		//switch back to main window and verify no windows are open anymore
-		myDriver.switchTo().window(main);
-		Assert.assertEquals(myDriver.getWindowHandles().size(), 1);
-		
-		//verify main window is still open
-		pageobjectmodelexample.verifyLinkLoaded("https://www.naukri.com/");
-		
-		myDriver.close();
 		
 	}
 
